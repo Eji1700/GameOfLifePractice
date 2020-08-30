af60b11e883a868d233038ae8d252129bc73e7ba
dungarDot <dungar@Dottys.com>
2020-08-30 13:40:45 -0700

    sprintf refactor

--- GameOfLife/Display.fs
+++ GameOfLife/Display.fs
@@ -1,15 +1,19 @@
 ﻿namespace Display
 open Types.Model
+
 module ConsoleOutput =
     
     let displayBoard (g:Game) =
-        for h in 0..g.Height do
+        [|for h in 0..g.Height do
             for w in 0..g.Width do
                 let c = Cell.getCell w h g
                 let x,_ = c.Position
                 if c.Status = Alive then
-                    printf "O"
+                    sprintf "O"
                 else 
-                    printf "-"
+                    sprintf "-"
                 if x = g.Width then 
-                    printf "\n"
+                    sprintf "\n"
+        |]
+        |> String.concat ""
+        |> printf "%s"
